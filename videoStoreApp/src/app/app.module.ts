837fd0a2a41cbfd7cae1cffe8de99f3ad2ccc43d
Sherwayne Walrond <sherwaynewalrond@gmail.com>
2018-11-30 19:52:07 -0500

    Implemented adminlogin and videolistadmin components. Static login layout added to adminlogin page.

--- videoStoreApp/src/app/app.module.ts
+++ videoStoreApp/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BsDropdownModule} from "ngx-bootstrap";
+import { Routes, RouterModule} from "@angular/router";
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { VideolistadminComponent } from './videolistadmin/videolistadmin.component';
 
+const routes: Routes = [
+  {path: 'adminlogin', component: AdminloginComponent},
+  {path: 'videolistadmin', component: VideolistadminComponent}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent,
+    VideolistadminComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ videoStoreApp/src/app/videolistadmin/videolistadmin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videolistadmin',
+  templateUrl: './videolistadmin.component.html',
+  styleUrls: ['./videolistadmin.component.css']
+})
+export class VideolistadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
