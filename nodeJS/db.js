c6f8be87055df0e00ef4486ac900d2a43dd4a13d
Creig Fortune <fortunephiri004@gmail.com>
2018-12-16 17:36:29 +0000

    change to the connection string. now connecting to remote mongo

--- nodeJS/db.js
+++ nodeJS/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/CRUDdb",{useNewUrlParser:true},err=>{
+// mongoose.connect("mongodb://localhost:27017/CRUDdb",{useNewUrlParser:true},err=>{
+    mongoose.connect("mongodb://admin:admin123@ds143340.mlab.com:43340/videostore",{useNewUrlParser:true},err=>{
     if(err){
         console.log("an error occured "+ JSON.stringify(err,undefined,2));
     }
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost:27017/CRUDdb",{useNewUrlParser:true},err=>
         console.log("Connection successful")
     }
 });
+
 
 
 module.exports =mongoose;
